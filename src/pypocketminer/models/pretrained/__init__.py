3492d908f74437b1698ebf15a372d549cb1c8e6e
Gavin John <gavinnjohn@gmail.com>
2025-08-09 18:39:32 -0400

    Fix that typo

--- src/pypocketminer/models/pretrained/__init__.py
+++ src/pypocketminer/models/pretrained/__init__.py
@@ -12,7 +12,7 @@ if spec is None or spec.origin is None:
 package_path = os.path.dirname(spec.origin)
 weights_dir = os.path.join(package_path, "models", "pretrained", "weights")
 
-pocketminer_v1_path = os.path.join(checkpoints_dir, "pocketminer_v1.weights.h5")
+pocketminer_v1_path = os.path.join(weights_dir, "pocketminer_v1.weights.h5")
 pocketminer_v1 = MQAModel(
     node_features=(8, 50),
     edge_features=(1, 32),
