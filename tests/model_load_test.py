5377adcb2f6b76a0a1e394bd4b611fdc0e1d0410
Gavin John <gavinnjohn@gmail.com>
2025-08-09 18:35:25 -0400

    Use weights instead of model checkpoints (more robust)

--- tests/model_load_test.py
+++ tests/model_load_test.py
@@ -1,21 +1,37 @@
 import os
 from pypocketminer.validate_performance_on_xtals import process_strucs
+from pypocketminer.models.pretrained import pocketminer_v1
 import mdtraj as md
 import numpy as np
 
-def test_model_loads_checkpoint():
-    from pypocketminer.models.pretrained import pocketminer_v1_status
-
-    pocketminer_v1_status.assert_consumed()
-
-def test_model_runs():
-    from pypocketminer.models.pretrained import pocketminer_v1 as model
-
+def test_model_runs_1JWP():
     pdb = md.load(os.path.join(os.path.dirname(os.path.abspath(__file__)), "data", "1JWP.pdb"))
 
     X, S, mask = process_strucs([pdb])
 
-    preds = np.array(model(X, S, mask, train=False, res_level=True))[0]
+    preds = np.array(pocketminer_v1(X, S, mask, train=False, res_level=True))[0]
+
+    assert np.all(0 <= preds)
+    assert np.all(preds <= 1)
+    assert len(preds) == 263
+
+def test_model_runs_1EXM():
+    pdb = md.load(os.path.join(os.path.dirname(os.path.abspath(__file__)), "data", "1EXM.pdb"))
+
+    X, S, mask = process_strucs([pdb])
+
+    preds = np.array(pocketminer_v1(X, S, mask, train=False, res_level=True))[0]
+
+    assert np.all(0 <= preds)
+    assert np.all(preds <= 1)
+    assert len(preds) == 263
+
+def test_model_runs_1NEP():
+    pdb = md.load(os.path.join(os.path.dirname(os.path.abspath(__file__)), "data", "1NEP.pdb"))
+
+    X, S, mask = process_strucs([pdb])
+
+    preds = np.array(pocketminer_v1(X, S, mask, train=False, res_level=True))[0]
 
     assert np.all(0 <= preds)
     assert np.all(preds <= 1)
